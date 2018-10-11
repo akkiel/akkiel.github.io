4b42ecc9e75fd7831cd76838fcb358f50fadb22d
akkiel <akielbasa@sbcglobal.net>
2018-10-11 09:08:09 -0500

    added movies

---
+++ movie-list/precache-manifest.75e425c4865b3efa94ee67f8503f6eac.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "212ee520d8cb9a83115a",
+    "url": "/static/js/main.212ee520.chunk.js"
+  },
+  {
+    "revision": "c7aa89cee206e09dc648",
+    "url": "/static/js/1.c7aa89ce.chunk.js"
+  },
+  {
+    "revision": "212ee520d8cb9a83115a",
+    "url": "/static/css/main.0482ec79.chunk.css"
+  },
+  {
+    "revision": "c7aa89cee206e09dc648",
+    "url": "/static/css/1.0c5f6b23.chunk.css"
+  },
+  {
+    "revision": "9f219a43b37cbb95a1f05e3be70c3cb0",
+    "url": "/index.html"
+  }
+];
