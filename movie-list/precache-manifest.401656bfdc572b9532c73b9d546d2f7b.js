f417afedc2614e1bf48d754165249602f084165e
akkiel <akielbasa@sbcglobal.net>
2018-10-11 22:22:59 -0500

    update lists

---
+++ movie-list/precache-manifest.401656bfdc572b9532c73b9d546d2f7b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "33fade6a15d988e9015c",
+    "url": "./static/js/main.33fade6a.chunk.js"
+  },
+  {
+    "revision": "e843f4a27159d3089318",
+    "url": "./static/js/1.e843f4a2.chunk.js"
+  },
+  {
+    "revision": "33fade6a15d988e9015c",
+    "url": "./static/css/main.0482ec79.chunk.css"
+  },
+  {
+    "revision": "e843f4a27159d3089318",
+    "url": "./static/css/1.0c5f6b23.chunk.css"
+  },
+  {
+    "revision": "b416c4714bb06ec587c3932daa8e8e72",
+    "url": "./index.html"
+  }
+];
--- movie-list/service-worker.js
+++ movie-list/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.85c8d5c5f91bc51a0c687e5ba393f8e8.js"
+  "./precache-manifest.401656bfdc572b9532c73b9d546d2f7b.js"
 );
 
 workbox.clientsClaim();
