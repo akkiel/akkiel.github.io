3c176aba99fcbebd8511faea9a72df6da5bbaeb2
akkiel <akielbasa@sbcglobal.net>
2018-10-20 11:27:44 -0500

    secured auth

---
+++ movie-list/precache-manifest.793331792f3178abd2b586b49af37023.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "6b60b9a0d10c4dcd2982",
+    "url": "./static/js/main.6b60b9a0.chunk.js"
+  },
+  {
+    "revision": "6f572810939d2250e2cc",
+    "url": "./static/js/1.6f572810.chunk.js"
+  },
+  {
+    "revision": "6b60b9a0d10c4dcd2982",
+    "url": "./static/css/main.0482ec79.chunk.css"
+  },
+  {
+    "revision": "6f572810939d2250e2cc",
+    "url": "./static/css/1.0c5f6b23.chunk.css"
+  },
+  {
+    "revision": "1bd0c0b6624dd364c6b8f203cc90b8a8",
+    "url": "./index.html"
+  }
+];
--- movie-list/service-worker.js
+++ movie-list/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.9deb87cda8fb41d5c1051f13533772e3.js"
+  "./precache-manifest.793331792f3178abd2b586b49af37023.js"
 );
 
 workbox.clientsClaim();
